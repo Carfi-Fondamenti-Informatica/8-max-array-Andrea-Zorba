ea151ed18207f2c8c7732eb64074de0810a1596c
Andrea-Zorba <92543460+Andrea-Zorba@users.noreply.github.com>
2021-12-10 21:18:41 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,20 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
-int main(){
-  return 0;
+int main() {
+    int a=0;
+    float num=0;
+    cin>> a;
+    if (a<=0){
+        return 0;
+    }
+   float numeri[a];
+    for (int i=0; i<a; i++){
+        num=0;
+        cin>> num;
+        numeri[1] = num;
+    }
+    cout<<maggiore(numeri,a)<<endl;
+    return 0;
 }
