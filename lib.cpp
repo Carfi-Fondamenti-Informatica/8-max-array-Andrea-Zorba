34c79f36ed480cb2a1f7a07047c5f7982e2384d9
Andrea-Zorba <92543460+Andrea-Zorba@users.noreply.github.com>
2021-12-10 21:20:06 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,12 @@
-
+#include "lib.h"
+using namespace std;
+float maggiore (float numeri[],int dim){
+    float b=0;
+    b=numeri [0];
+    for (int j=0; j<dim; j++){
+        if (b<numeri[j]){
+            b=numeri[j];
+        }
+    }
+    return b;
+}
